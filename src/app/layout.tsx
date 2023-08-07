2fde6d22acc196a60a5f745033438042ce3c5458
bill <>
2023-08-08 00:25:07 +0800

    feat: add header Alert

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ThemeProvider } from '@/theme'
+import { Alert, Typography } from '@mui/material'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +21,24 @@ export default function RootLayout({
     <ThemeProvider>
       <html lang="en">
         <body className={inter.className}>
+          <Alert sx={{
+            backgroundColor: '#000',
+            borderRadius: 0,
+            display: 'flex',
+            justifyContent: 'center',
+          }} icon={false} severity="success">
+            <Typography color={'#fff'} sx={{fontSize: {xs: 14, sm: 16}}}>
+              KRAV token address is {' '}
+              <a style={{color:'#fff'}} target='_blank' href='https://basescan.org/token/0xbE3111856e4acA828593274eA6872f27968C8DD6'>
+                0xbE31....8DD6
+              </a>
+              {' '}, follow our{' '}
+              <a style={{color:'#fff'}} target='_blank' href='https://twitter.com/kravtrade'>
+                Twitter
+              </a>
+              {' '}and beware of any scam tokens!
+            </Typography>
+          </Alert>
           <Navbar />
           {children}
         </body>
